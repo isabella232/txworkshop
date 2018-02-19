bd0060413a25331f50ad2008b5ac314657c83b84
Austin Rutherford <austinrutherford@gmail.com>
2018-02-19 13:18:55 -0600

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -9,14 +9,14 @@ mysqld_multi start 3
 mysqld_multi start 4
 
 ### These starts do not happen immediate. This sleep is a low tech way to wait. Needs to be replaced with a proper check that loops for all four databases are up
-sleep 10
+sleep 15
 
 mysql -h 127.0.0.1 -P 33061 < ./scripts/mariadb.sql 
 mysql -h 127.0.0.1 -P 33062 < ./scripts/mariadb.sql 
 mysql -h 127.0.0.1 -P 33063 < ./scripts/mariadb.sql 
 mysql -h 127.0.0.1 -P 33064 < ./scripts/mariadb.sql 
 
-sleep 10
+sleep 15
 mysql -h 127.0.0.1 -P 33061 < ./scripts/seed.sql
 
 
