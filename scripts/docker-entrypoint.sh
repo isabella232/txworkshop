913dcb4b8872381ebeb5fbd5228ad2675bf66d40
Austin Rutherford <austinrutherford@gmail.com>
2018-03-31 02:38:14 -0500

    Update docker-entrypoint.sh

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -3,6 +3,7 @@ mysqld_multi start 1
 mysqld_multi start 2
 mysqld_multi start 3
 mysqld_multi start 4
+mysql -h 127.0.0.1 -P 33061 < /usr/share/mysql/install_spider.sql
 sudo -u maxscale /usr/bin/maxscale &
 exec "$@"
 bash
