3489d2d4e7b3eb73a70207ca7a2c1065fe135ffd
Austin Rutherford <austinrutherford@gmail.com>
2018-11-03 12:53:11 -0500

    Update mariadb.sql

--- scripts/mariadb.sql
+++ scripts/mariadb.sql
@@ -1,5 +1,5 @@
 
-create user 'maxuser'@'%' identified by 'password';
+create user 'maxuser'@'%' identified by 'maxpwd';
 grant SELECT on mysql.user to 'maxuser'@'%';
 GRANT SELECT ON mysql.db TO 'maxuser'@'%';
 GRANT SELECT ON mysql.tables_priv TO 'maxuser'@'%';
@@ -10,7 +10,7 @@ GRANT CREATE, ALTER, SELECT, INSERT, UPDATE, DELETE ON *.* TO 'maxuser'@'%';
 GRANT REPLICATION CLIENT on *.* to 'maxuser'@'%';
 GRANT REPLICATION SLAVE ON *.* TO 'maxuser'@'%';
 
-create user 'maxuser'@'localhost' identified by 'password';
+create user 'maxuser'@'localhost' identified by 'maxpwd';
 grant SELECT on mysql.user to 'maxuser'@'localhost';
 GRANT SELECT ON mysql.db TO 'maxuser'@'localhost';
 GRANT SELECT ON mysql.tables_priv TO 'maxuser'@'localhost';
@@ -30,7 +30,7 @@ IF @@server_id  > 1 THEN
   CHANGE MASTER TO
     MASTER_HOST='localhost',
     MASTER_USER='maxuser',
-    MASTER_PASSWORD='password',
+    MASTER_PASSWORD='maxpwd',
     MASTER_PORT=33061,
     MASTER_CONNECT_RETRY=10,
     master_use_gtid=current_pos;
