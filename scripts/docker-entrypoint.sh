c0c6d0787027f8668aa9f872c8c0468278b71472
Austin Rutherford <austinrutherford@gmail.com>
2019-08-27 01:58:29 -0400

    Update docker-entrypoint.sh

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -3,7 +3,8 @@ mysqld_multi start 1
 mysqld_multi start 2
 mysqld_multi start 3
 mysqld_multi start 4
-mysql -S /var/lib/mysql/1/mysql.sock -P 33061 < /usr/share/mysql/install_spider.sql
+#mysql -S /var/lib/mysql/1/mysql.sock -P 33061 < /usr/share/mysql/install_spider.sql
 sudo -u maxscale /usr/bin/maxscale &
+maxctrl call command mariadbmon reset-replication TheMonitor server1
 exec "$@"
 bash
