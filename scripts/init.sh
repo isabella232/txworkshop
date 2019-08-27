91833e7601ebd1161b1955720e1aa68501a32658
Austin Rutherford <austinrutherford@gmail.com>
2019-08-27 12:52:49 -0400

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -8,15 +8,28 @@ mysqld_multi start 2
 mysqld_multi start 3
 mysqld_multi start 4
 
+
 ### These starts do not happen immediate. This sleep is a low tech way to wait. Needs to be replaced with a proper check that loops for all four databases are up
-sleep 15
+echo "Sleeping for 5..."
+sleep 5
+mysqld_multi report
 
+echo "Init MariaDB 1..."
 mysql -S /var/lib/mysql/1/mysql.sock -P 33061 < ./scripts/mariadb.sql 
+echo "Init MariaDB 2..."
 mysql -S /var/lib/mysql/2/mysql.sock -P 33062 < ./scripts/mariadb.sql 
+echo "Init MariaDB 3..."
 mysql -S /var/lib/mysql/3/mysql.sock -P 33063 < ./scripts/mariadb.sql 
+echo "Init MariaDB 4..."
 mysql -S /var/lib/mysql/4/mysql.sock -P 33064 < ./scripts/mariadb.sql 
+sleeo 2
+sudo -u maxscale /usr/bin/maxscale &
+sleep 2
+echo "Setting up replication..."
+maxctrl call command mariadbmon reset-replication TheMonitor server1
 
+echo "Init Seed..."
 sleep 5
 mysql -S /var/lib/mysql/1/mysql.sock -P 33061 < ./scripts/seed.sql
 
-maxctrl call command mariadbmon reset-replication TheMonitor server1
+echo "Init.sh complete!"
